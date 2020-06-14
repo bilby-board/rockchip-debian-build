5c3e26d9a2a6a5856df20fe657eec5e1c60534cb
Alejandro Mery <amery@jpi.io>
2020-06-14 23:30:37 +0000

    docker: add sudo support via `x -r`
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- docker/docker-run.sh
+++ docker/docker-run.sh
@@ -2,8 +2,17 @@
 
 set -eux
 
+# $WS/run.sh [-r] <cmd>
+#
 WS="$(dirname "$0")"
 DOCKER_DIR="$(dirname "$(readlink -f "$0")")"
+
+if [ "x${1:-}" = "x-r" ]; then
+	SUDO=yes
+	shift
+else
+	SUDO=
+fi
 
 F="$DOCKER_DIR/Dockerfile"
 if [ ! $F.in -ot $F ]; then
@@ -21,4 +30,4 @@ if ! docker images | grep -q "$(echo "^$IMAGE:" | sed -e 's|:|[ \\t]\\+|g')"; th
 fi
 
 export DOCKER_DIR
-exec "$PD/run.sh" "$DOCKER_DIR/run.sh" "$@"
+exec "$PD/run.sh" ${SUDO:+-r} "$DOCKER_DIR/run.sh" "$@"
