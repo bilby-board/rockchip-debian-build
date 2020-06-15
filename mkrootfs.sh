b77b9d78cf0a7cdc37f3da57f58ade24e5bcebde
Alejandro Mery <amery@jpi.io>
2020-06-15 00:32:49 +0000

    mkrootfs: separate qemu-*-static injection from debootstrap
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- mkrootfs.sh
+++ mkrootfs.sh
@@ -15,9 +15,18 @@ armhf) QEMU=/usr/bin/qemu-arm-static ;;
 esac
 
 ROOTFS="$O/rootfs/$DISTRO-$DISTRO_VERSION-$ARCH"
-if [ ! -d "$ROOTFS" ]; then
+
+# inject qemu-*-static before `debootstrap`
+#
+if [ ! -x "$ROOTFS$QEMU" ]; then
 	mkdir -p "$ROOTFS${QEMU%/*}"
 	cp "$QEMU" "$ROOTFS$QEMU"
+fi
+
+# populate rootfs
+#
+if [ ! -x $ROOTFS/bin/sh ]; then
 	debootstrap --arch $ARCH $DISTRO_VERSION "$ROOTFS" "$DISTRO_URL"
 fi
+
 du -sh "$ROOTFS"
