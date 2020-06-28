96769c7f5dc634add83d0342c55331a3994b9514
Alejandro Mery <amery@jpi.io>
2020-06-28 15:08:02 +0000

    run: chroot if called from within a rootfs
    Signed-off-by: Alejandro Mery <amery@jpi.io>

---
+++ scripts/chroot.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+chroot "$1"
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,33 @@
 #!/bin/sh
 
+set -eu
+
+# we are $WS/scripts/run.sh
+SCRIPTS_DIR="$(dirname "$0")"
+WS="$(cd "$SCRIPTS_DIR/.." && pwd)"
+
+# are we at a rootfs to chroot into?
+#
+find_rootfs() {
+	local root="$1" dir="${2:-$PWD}" d=
+
+	if [ "$root" = "$dir" ]; then
+		:
+	elif [ -e "$dir/etc/passwd" ]; then
+		echo "$dir"
+	else
+		find_rootfs "$root" "$(cd "$dir/.." && pwd)"
+	fi
+}
+
+ROOTFS="$(find_rootfs "$WS")"
+
+if [ -d "$ROOTFS" ]; then
+	# run inside chroot
+	exec "$SCRIPTS_DIR/chroot.sh" "$ROOTFS" "$@"
+fi
+
+# not chrooted
 [ $# -gt 0 ] || set -- /bin/bash
 
 exec "$@"
